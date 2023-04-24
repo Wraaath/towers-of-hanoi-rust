255367b43bf57cb8fb9e838d6bffff024f4d407e
Wraaath <mads@wraaath.com>
2023-04-24 12:28:15 +0200

    peg init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn peg_init() {
+    let mut peg_1: Vec<u16> = vec![6, 5, 4, 3, 2, 1];
+    let mut peg_2: Vec<u16> = vec![];
+    let mut peg_3: Vec<u16> = vec![];
+    println!("peg_1: {:?} 
+             \npeg_2: {:?} 
+             \npeg_3: {:?}"
+             , peg_1, peg_2, peg_3);
+}
+
+fn peg_moving() {
+    println!("hej")
 }
